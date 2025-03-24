b33ed71d1cdf9cc9d16ed5208b96db1bde100c51
이유석 <yusuk199@naver.com>
2025-03-24 11:33:35 +0900

    오래된 pdf 파일 정리

--- automated file sorting/file_sort.py
+++ automated file sorting/file_sort.py
@@ -3,7 +3,8 @@ import shutil
 from datetime import datetime, timedelta
 from pathlib import Path
 
-TARGET_FOLDER = Path("C:/Users/USER/Desktop/old_files")
+ROOT_DIR = Path("C:/Users/USER/Downloads")
+DEST_FOLDER = Path("C:/Users/USER/Desktop/old_files")
 DAYS_THRESHOLD = 7
 NOW = datetime.now()
 
@@ -11,30 +12,32 @@ def get_destination(file: Path):
     name = file.name.lower()
     ext = file.suffix.lower()
 
-    if "img" in name:
-        return TARGET_FOLDER / "Images"
-    elif ext == ".jpg":
-        return TARGET_FOLDER / "Pictures"
-    elif ext == ".pdf":
-        return TARGET_FOLDER / "Documents"
-    elif ext == ".zip":
-        return TARGET_FOLDER / "Archives"
+    
+    if ext == ".pdf":
+        return DEST_FOLDER / "Documents"
     return None
 
 def organize_files():
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 파일 정리 시작")
-    for file in TARGET_FOLDER.iterdir():
-        if file.is_file():
-            last_modified = datetime.fromtimestamp(file.stat().st_mtime)
-            if NOW - last_modified > timedelta(days=DAYS_THRESHOLD):
-                dest = get_destination(file)
-                if dest:
-                    dest.mkdir(exist_ok=True)  
-                    try:
-                        shutil.move(str(file), str(dest / file.name))
-                        print(f"Moved: {file.name} → {dest.name}/")
-                    except Exception as e:
-                        print(f"Error moving {file.name}: {e}")
+    for dirpath, _, filenames in os.walk(ROOT_DIR):
+        for fname in filenames:
+            try:
+                file_path = Path(dirpath) / fname
+                if not file_path.exists() or not file_path.is_file():
+                    continue
+                last_modified= datetime.fromtimestamp(file_path.stat().st_mtime)
+                if NOW - last_modified > timedelta(days=DAYS_THRESHOLD):
+                    dest = get_destination(file_path)
+                    if dest:
+                        dest.mkdir(parents=True, exist_ok=True)
+                        shutil.move(str(file_path), str(dest/file_path.name))
+                        print(f"Moved: {file_path} -> {dest}")
+
+            except PermissionError:
+                continue
+
+            except Exception as e:
+                    print(f"Error: {file_path} - > {e}")
     print("정리 완료!\n")
 
 if __name__ == "__main__":
